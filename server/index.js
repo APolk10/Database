4b8e8aef2a6c93769f7af8d87f519fa010889439
Yari Torres Nicola <ybtorres9@gmail.com>
2022-10-04 11:20:05 -0600

    edited index file

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.post('/addjob', (req, res) => {
     .catch((err) => res.status(400).send(err));
 })
 app.get('/getAllJobs', (req, res) => {
-  controller.getAllJobs()
+  jobControllers.getAllJobs()
     .then((response) => res.status(200).send(response))
     .catch((err) => res.status(400).send(response));
 })
